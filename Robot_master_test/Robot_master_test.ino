4b5c397c27e01d5e96136662adcbd8d87511876a
Matthew Fegan <matthew@ifspace.org>
2017-10-18 23:10:40 -0400

    Changed the timeout on the servo steps
    This should effectively make the servo move more slowly, keeping the sign from slipping on its axle.

--- Robot_master_test/Robot_master_test.ino
+++ Robot_master_test/Robot_master_test.ino
@@ -168,14 +168,14 @@ void playfile(char *name) {
   for (pos = 90; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(30);                       // waits 15ms for the servo to reach the position
   }
  }
 
  void sweepleft() {
   for (pos = 120; pos >= 60; pos -= 1) { // goes from 180 degrees to 0 degrees
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(30);                       // waits 15ms for the servo to reach the position
   }
  }
 
@@ -183,7 +183,7 @@ void playfile(char *name) {
   for (pos = 60; pos <= 120; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(30);                       // waits 15ms for the servo to reach the position
   }
  }
 
@@ -191,7 +191,7 @@ void playfile(char *name) {
   for (pos = 60; pos <= 90; pos += 1) { // goes from 0 degrees to 180 degrees
     // in steps of 1 degree
     myservo.write(pos);              // tell servo to go to position in variable 'pos'
-    delay(15);                       // waits 15ms for the servo to reach the position
+    delay(30);                       // waits 15ms for the servo to reach the position
   }
  }
  
